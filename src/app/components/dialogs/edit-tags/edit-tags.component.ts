0b430e323d80a85e335ed5d3c381e124224b5292
Nathan Foss <fossphorus@gmail.com>
2024-06-18 20:15:59 -0500

    Init edit-tags component

---
+++ src/app/components/dialogs/edit-tags/edit-tags.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-tags',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-tags.component.html',
+  styleUrl: './edit-tags.component.scss'
+})
+export class EditTagsComponent {
+
+}
