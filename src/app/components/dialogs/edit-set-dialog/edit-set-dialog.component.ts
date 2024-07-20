e83a28ffbb953bcf573d92aec63573a752c0a1b7
Nathan Foss <nathankfoss@gmail.com>
2024-07-19 19:57:31 -0500

    Styling for edit-set

--- src/app/components/dialogs/edit-set-dialog/edit-set-dialog.component.ts
+++ src/app/components/dialogs/edit-set-dialog/edit-set-dialog.component.ts
@@ -1,17 +1,40 @@
 import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import {
+    MAT_DIALOG_DATA,
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogRef,
+    MatDialogTitle,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ExerciseSet } from '../../../models/workout.model';
 
 @Component({
-  selector: 'app-edit-set-dialog',
-  standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule, MatDialogTitle, MatDialogContent, MatDialogActions],
-  templateUrl: './edit-set-dialog.component.html',
-  styleUrl: './edit-set-dialog.component.scss'
+    selector: 'app-edit-set-dialog',
+    standalone: true,
+    imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        FormsModule,
+        MatDialogTitle,
+        MatDialogContent,
+        MatDialogActions,
+    ],
+    templateUrl: './edit-set-dialog.component.html',
+    styles: `
+        .main-container {
+            display: flex;
+            flex-direction: column;
+        }
+          .actions-container { 
+            display: flex; 
+            justify-content: flex-end; 
+            padding: 0 20px 20px 0; 
+        }`,
 })
 export class EditSetDialogComponent {
     constructor(
