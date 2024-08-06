6249ebaa6c854e653402053cd9825225f11b6d66
Nathan Foss <nathankfoss@gmail.com>
2024-08-05 21:01:30 -0500

    Snackbar component consolidation

--- src/app/components/snackbars/generic-snackbar/generic-snackbar.component.ts
+++ src/app/components/snackbars/generic-snackbar/generic-snackbar.component.ts
@@ -2,24 +2,71 @@ import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_SNACK_BAR_DATA, MatSnackBar } from '@angular/material/snack-bar';
 
+export type GenericSnackBarStatus = 'success' | 'error';
+
 export interface GenericSnackBarData {
-    message?: string,
-    showDismissButton?: boolean
+    status: GenericSnackBarStatus;
+    message?: string;
+    showDismissButton?: boolean;
 }
 
 @Component({
-  selector: 'app-generic-snackbar',
-  standalone: true,
-  imports: [MatButtonModule],
-  templateUrl: './generic-snackbar.component.html',
-  styleUrl: './generic-snackbar.component.scss',
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-generic-snackbar',
+    standalone: true,
+    imports: [MatButtonModule],
+    template: `
+        <div class="generic-snack-bar-container">
+            <div class="generic-snack-bar-message-container">
+                {{ message }}
+            </div>
+            @if (data.showDismissButton) {
+            <div>
+                <button mat-flat-button (click)="onDismiss()">Dismiss</button>
+            </div>
+            }
+        </div>
+    `,
+    styles: `
+        .generic-snackbar-success {
+            background: green;
+            color: white;
+            min-height: 40px;
+            margin: 15px;
+            font-size: 14px;
+        }
+        .generic-snackbar-error {
+            background: red;
+            color: white;
+            min-height: 40px;
+            margin: 15px;
+            font-size: 14px;
+        }
+        .generic-snack-bar-container {
+            display: flex;
+            justify-content: center;
+
+            .generic-snack-bar-message-container {
+                padding: 0 10px 0 0;
+            }
+        }
+    `,
+    encapsulation: ViewEncapsulation.None,
 })
 export class GenericSnackbarComponent {
+    message: string = '';
+
     constructor(
         private snackBar: MatSnackBar,
         @Inject(MAT_SNACK_BAR_DATA) public data: GenericSnackBarData
-    ) {}
+    ) {
+        if (data.message) {
+            this.message = data.message;
+        } else if (data.status === 'success') {
+            this.message = 'Success!';
+        } else if (data.status === 'error') {
+            this.message = 'An error occurred.';
+        }
+    }
 
     onDismiss() {
         this.snackBar.dismiss();
